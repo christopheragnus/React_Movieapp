e12e7c837026a0fa89154540898dc4edddf65579
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-07-13 13:05:28 +1000

    added Welcome Home component

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// statless functional component
+export const Home = props => (
+  <div className="home">
+    <h2>Welcome home, React friends</h2>
+  </div>
+
+)
+
+export default Home;
